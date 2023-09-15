4beb1dcb7752bc4dae67ac6162ea48619099c6e4
M4XG4RC1A <maxgarciae.c@hotmail.com>
2023-09-14 18:26:21 -0600

    Challenges files add

--- CodigoFinal/Modos/Modes/TE/ReadModes.m
+++ CodigoFinal/Modos/Modes/TE/ReadModes.m
@@ -1,15 +1,32 @@
+
 %% getmodes
+clc;
+
 Xs = [300, 475, 650, 825, 1000];
 Ys = [100, 325, 550, 775, 1000];
-x = Xs(3);
-y = Ys(3);
 Ls = [510,750,1000,1530,1555,1580];
+l = Ls(6);
+y = Ys(5);
 
-disp("x: "+string(x))
+
 disp("y: "+string(y))
+disp("l: "+string(l))
 
-for l = Ls
-    load("Waveguide"+string(x)+"_"+string(y)+"_"+string(l)+".mat");
-    disp("l: "+string(l))
-    disp(nmodes)
+ex = 0;
+nx = "";
+for x = Xs
+    try
+        load("Waveguide"+string(x)+"_"+string(y)+"_"+string(l)+".mat");
+    catch
+        nmodes = 0;
+        %ex = 1;
+        %nx = nx+string(x)+", ";
+    end
+    disp("x: "+string(x))
+    disp("  Modos: "+string(nmodes))
+end
+
+if ex==1
+    disp("Alguno no existio");
+    disp(nx);
 end
--- CodigoFinal/Modos/Modes/getTE.m
+++ CodigoFinal/Modos/Modes/getTE.m
@@ -1,18 +1,20 @@
 %% Read TE
-xs = [475,825,1000];
-ys = [775,325,325];
-sel = 3;
+clc;
+
+xs = [825,1000];
+ys = [325,325];
+sel = 2;
 
 x = xs(sel);
 y = ys(sel);
 
-Ls = [510,750,1000,1530,1555,1580];
+Ls = [510,750,1000,1555,1580];
 disp("x: "+string(x))
 disp("y: "+string(y))
 for l = Ls
     disp("lambda: "+string(l))
     load("./TE/Waveguide"+string(x)+"_"+string(y)+"_"+string(l)+".mat");
-    disp("  TE: "+string(TE))
+    disp("  TE: "+string(TE*100))
     load("./overlapTE/Waveguide"+string(x)+"_"+string(y)+"_"+string(l)+".mat");
-    disp("  overlapTE: "+string(modeOverlap.fraction))
+    disp("  overlapTE: "+string(modeOverlap.fraction*100))
 end
